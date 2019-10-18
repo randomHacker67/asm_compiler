300b2376644a487ea3b67f0f317422be61191f45
JovalTHEJoseph <56718577+JovalTHEJoseph@users.noreply.github.com>
2019-10-18 20:46:14 +0530

    Update Compiler.sh

--- Compiler.sh
+++ Compiler.sh
@@ -1,1 +1,70 @@
 #!/bin/bash
+function build() {
+    set -x
+    nasm $ASMFLAGS -o asmcc.o start.asm || exit $?
+    ld -o asmcc asmcc.o || exit $?
+    { set +x; } 2> /dev/null # Disable logging
+}
+
+function run() {
+    set -x
+    ./asmcc examples/test.c
+}
+
+function run_with_debugger() {
+    set -x
+    gdb --args asmcc examples/test.c
+}
+
+function run_with_backtrace() {
+    set -x
+    nm asmcc -l > syms
+    { ./asmcc examples/test.c; exit $?; } | \
+        { tee log | sed -e '/fn>>/d'; } 2>/dev/null
+    { set +x; } 2> /dev/null # Disable logging
+
+    # Print out the backtrace if it is present
+    sed -e '/fn>>/ !d' -e 's/fn>> //' log | \
+        xargs -I pat -n1 grep pat syms
+}
+
+# defaults
+RUN=yes
+DEBUG=yes
+DEBUGGER=no
+BACKTRACE=yes
+ASMFLAGS="-w+all -f elf64"
+
+# Configuration
+export REVISION=$(git rev-parse --short HEAD)
+for ARG in $*; do
+    if [ "$ARG" = "--release" ]; then
+        ASMFLAGS="-w+all -f elf64"
+        RUN=no
+        DEBUG=no
+        BACKTRACE=no
+    fi
+    if [ "$ARG" = "--debug" ]; then
+        DEBUGGER=yes
+    fi
+done
+
+if [ $DEBUG = yes ]; then
+    ASMFLAGS="$ASMFLAGS -g -F stabs -O0"
+fi
+
+if [ $BACKTRACE = yes ]; then
+    ASMFLAGS="$ASMFLAGS -dBACKTRACE"
+fi
+
+# Debug Build & Run
+build
+if [ $RUN = yes ]; then
+    if [ $DEBUGGER = yes ]; then
+        run_with_debugger
+    elif [ $BACKTRACE = yes ]; then
+        run_with_backtrace
+    else
+        run
+    fi
+fi
